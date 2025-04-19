bf5c623da314a21e64eb954f2908e59ab825dcf1
optima <139846311+Rio-0912@users.noreply.github.com>
2025-04-19 15:59:38 +0530

    inital react commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Dashboard from './Pages/Dashboard'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Dashboard/>
+      </div>
+    </>
+  )
+}
+
+export default App
